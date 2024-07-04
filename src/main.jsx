a6b627ab955a9e8c578732b0128b626bc919d416
criston04 <josecamarenameza0@gmail.com>
2024-07-03 19:49:27 -0500

    actualizacion del 03

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import WheaterApp from '../src/wheaterApp';
-import '../src/estilos/weather-style.css'
+import WheaterApp from './wheaterApp';
+import './estilos/weather-style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
