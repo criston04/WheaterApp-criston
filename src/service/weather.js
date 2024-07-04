3a2b13859b4837d0bb9a0eadb80b8953a4f48fa7
criston04 <josecamarenameza0@gmail.com>
2024-07-03 21:55:35 -0500

    climaso

--- src/service/weather.js
+++ src/service/weather.js
@@ -1,10 +1,9 @@
 import {ajax} from '../tools/ajax';
 
-
 export const getCityWeather = async city => {
     const optionsRequest = {
         method: "GET",
-        url: "http://api.openweathermap.org/data/2.5/weather",
+        url: "https://api.openweathermap.org/data/2.5/weather", // Cambia http por https
         params: {
             q: city,
             appid: "21e3195e9bfeb6879e788ec605b09ab0",
