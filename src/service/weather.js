4419126221610aff75ed65306507cad386f73b92
criston04 <josecamarenameza0@gmail.com>
2024-07-10 12:04:26 -0500

    nueva apikey

--- src/service/weather.js
+++ src/service/weather.js
@@ -3,10 +3,10 @@ import {ajax} from '../tools/ajax';
 export const getCityWeather = async city => {
     const optionsRequest = {
         method: "GET",
-        url: "https://api.openweathermap.org/data/2.5/weather", // Cambia http por https
+        url: "https://api.openweathermap.org/data/2.5/weather", // Cambia http por https api.openweathermap.org/data/2.5/weather
         params: {
             q: city,
-            appid: "67d44c4765a2e43d077a024605351577",
+            appid: "ecaf3c4512ad13174cc8f83cb0a863c3",
             units: "metric" // Grados centígrados
         }
     };
